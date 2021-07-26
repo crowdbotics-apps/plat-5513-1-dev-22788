2fa4b2731e99947c1084c26367aba158d7526c8e
Crowdbotics <team@crowdbotics.com>
2021-07-26 05:18:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/plat_5513_1_dev_22788/MainActivity.java
@@ -0,0 +1,15 @@
+package com.plat_5513_1_dev_22788;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "plat_5513_1_dev_22788";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "plat_5513_1_dev_22788Identifier",
+  url: "https://plat_5513_1_dev_22788Identifier.botics.co",
+  api: "https://plat_5513_1_dev_22788Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
